d37d58a97f56082c6f26be1a546861a055e3c163
Panagiotis Kontoeidis <panos.kontoeidis@gmail.com>
2024-10-31 16:48:47 +0200

    first push

---
+++ src/main/java/backend/kassignment/KAssignmentApplication.java
@@ -0,0 +1,13 @@
+package backend.kassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/kassignment/KAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.kassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
